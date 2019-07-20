bd354af66a611e7f994880d19adeae395a725b18
sandra429 <53117446+sandra429@users.noreply.github.com>
2019-07-20 15:27:42 +0200

    Update whatsapp.php

--- tests/whatsapp.php
+++ tests/whatsapp.php
@@ -15,8 +15,8 @@ function fgets_u($pStdn) {
 }
 
 $nickname = "WhatsAPI Test";
-$sender =   "34659730605"; // Mobile number with country code (but without + or 00)
-$imei =     "351565053325764"; // MAC Address for iOS IMEI for other platform (Android/etc)
+$sender =   "4917622857364"; // Mobile number with country code (but without + or 00)
+$imei =     "863034047349902"; // MAC Address for iOS IMEI for other platform (Android/etc)
 
 $countrycode = substr($sender, 0, 2);
 $phonenumber=substr($sender, 2);
